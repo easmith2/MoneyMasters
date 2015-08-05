4266afa01ca094263f380d0b112934e67bc8d338
Andrew T. poe <techapoe@gmail.com>
2015-08-05 11:50:15 -0400

    Now rendering a budgets div and a transactionn div on app home

--- app/assets/javascripts/App.react.js
+++ app/assets/javascripts/App.react.js
@@ -1,13 +1,43 @@
 var React = require('react');
 var Router = require('director').Router;
+var Budgets = require('./Budgets.react');
+var Transactions = require('./Transactions.react');
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      currentView: this._show_user_budget
+    }
+  },
+
+  componentWillMount: function() {
+    this._initRouter();
+    // this.setState = {
+    //   currentView: this.show_user_budget
+    // }
+  },
+
+  // ._fetchTask: function() {
+  //     request
+  //       .get(url)
+  //       .set('Accept', 'application/json')
+  //       .end(this._handleFetch);
+  //   },
+  //
+  //   ._handleFetch: function() {
+  //     if (err) {
+  //       window.alert('OMGEEE');
+  //       return;
+  //     }
+  //     this.setState({ items: res.body });
+  //   },
+
 
   _initRouter: function() {
     var self = this;
     console.log('Initializing Router');
     self.router = Router({
-      '/'            : self._displayIndex,
+      '/'            : self._show_user_budget,
     });
     self.router.configure({ html5history: true });
     self.router.init();
@@ -17,11 +47,21 @@ var App = React.createClass({
     this.router.setRoute(href);
   },
 
+  _show_user_budget: function() {
+    return (
+      <div className="show_user_budget">
+        <Budgets />
+        <Transactions />
+      </div>
+    )
+  },
+
   render: function() {
     console.log('Rendering App Class');
+    var insert = this.state.currentView();
     return (
       <div className="app">
-        "Halooooooo....."
+        { insert }
       </div>
     );
   }
