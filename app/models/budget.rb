5a9f70bdb505fbaa36486f258a1c784b391edc80
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-04 23:30:20 -0400

    fixed syntax error in budget and category models

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,7 +1,7 @@
 class Budget < ActiveRecord::Base
   belongs_to :user
   has_many :budgetCategories
-  has_many :categories, through :budgetCategories
+  has_many :categories, through: :budgetCategories
   has_many :transactions
 
   validates :title, presence: true
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   belongs_to :user
   has_many :budgetCategories
-  has_many :budgets, through :budgetCategories
+  has_many :budgets, through: :budgetCategories
   has_many :transactions
 
 end
