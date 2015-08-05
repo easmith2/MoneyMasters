b024a49306c75ac77d7a3a68dbea950bd4f6dade
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-05 08:20:28 -0400

    added user controller tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
   def index
+    # respond_to do |format|
+    #   format.html
+    #   format.json { @users = User.all }
+    # end
   end
 
   def create
+    # @user = User.new(user_params)
+    # respond_to do |format|
+    #   if user.save
+    #     format.json { render json: {}, status: 200 }
+    #   else
+    #     format.json { render json: @task.errors, status: 422 }
+    #   end
+    # end
   end
+
+
+
+private
+  def user_params
+    params.require(:user).permit(:username)
 
 end
