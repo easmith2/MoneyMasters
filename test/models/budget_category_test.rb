c96527bdf79a508f7248ccb1d9a154f2ceac7b75
Andrew T. poe <techapoe@gmail.com>
2015-08-04 23:48:25 -0400

    Created relationships in the fixtures

--- test/models/budget_category_test.rb
+++ test/models/budget_category_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class BudgetCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @rent = budget_categories(:rent)
+    @food = budget_categories(:food)
+  end
+
+  test "fixture is valid" do
+    assert @rent.valid?
+    assert @food.valid?
+  end
+
+
 end
