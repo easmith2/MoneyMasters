d161c373361eacfb1394f9072644d1bc01280ac3
Andrew T. poe <techapoe@gmail.com>
2015-08-05 16:11:34 -0400

    All tests not skipped currently pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        format.json { render json: status: 200 }
+        format.json { render nothing: true, status: 200 }
       else
-        format.json { render json: status: 422 }
+        format.json { render nothing: true, status: 422 }
       end
     end
   end
