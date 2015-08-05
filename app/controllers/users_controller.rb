2ac53a169318b063831d36deff47f85431db99dd
Andrew T. poe <techapoe@gmail.com>
2015-08-05 13:56:42 -0400

    Added link to home/index view that loads user/id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,14 @@
 class UsersController < ApplicationController
-  def index
-    # respond_to do |format|
-    #   format.html
-    #   format.json { @users = User.all }
-    # end
+  def show
   end
 
   def create
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        format.json { render json: {}, status: 200 }
+        format.json { render json: status: 200 }
       else
-        format.json { render json: @task.errors, status: 422 }
+        format.json { render json: status: 422 }
       end
     end
   end
