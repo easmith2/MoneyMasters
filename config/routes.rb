e5486e8210fb9136e1dbacec230786d26813844d
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-05 13:20:43 -0400

    now including show for users and budgets

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :users, except: [:index, :show, :new, :edit] do
-    resources :budgets, except: [:show, :new, :edit]
+  resources :users, except: [:index] do
+    resources :budgets, except: [:new, :edit]
     resources :transactions, except: [:show, :new, :edit]
     resources :categories, except: [:show, :new, :edit]
   end
