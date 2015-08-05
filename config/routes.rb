0f136589aac699e90c6b76ef67f2762b53cf8090
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-05 13:14:11 -0400

    now including an index route for budgets

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users, except: [:index, :show, :new, :edit] do
-    resources :budgets, except: [:index, :show, :new, :edit]
+    resources :budgets, except: [:show, :new, :edit]
     resources :transactions, except: [:show, :new, :edit]
     resources :categories, except: [:show, :new, :edit]
   end
