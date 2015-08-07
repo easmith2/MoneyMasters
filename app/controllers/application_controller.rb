4871fc6f2e4f6d54aa15fca21add252823273ac9
Andrew T. poe <techapoe@gmail.com>
2015-08-07 16:34:30 -0400

    Added user athentication. No changes to transactions controller or React yet made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 
-  def authenticate_user!
-    if user_signed_in?
-      user_path(current_user.id)
-    end
-  # def after_sign_in_path_for(resource)
-  #   current_user_path
-  # end
-
-  # def after_sign_out_path_for(resource)
-  #   root_path
-  # end
+  def after_sign_up_path_for(resource)
+    user_path(resource)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,11 @@
 class UsersController < ApplicationController
 
   def show
-    binding.pry
-    @user = current_user
-  end
-
-  def destroy
-    binding.pry
-    session[:user_id] = nil
-    current_user
-    redirect_to root_path
-  end
-
-  def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user_path(user.id)
+    if current_user
+      @user = current_user
     else
-      redirect_to new_user_registration_path
+      redirect_to root_path
     end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password)
   end
 
 end
