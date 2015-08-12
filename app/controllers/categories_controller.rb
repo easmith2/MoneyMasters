38bbdac3d0a8c320f0000c8659ff22d8b67e3dcd
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-08-12 15:09:03 -0400

    added before_action authentication to application controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,59 +1,43 @@
 class CategoriesController < ApplicationController
 
   def index
-    if current_user
-      respond_to do |format|
-        format.html { render template: 'users/show' }
-        format.json { @categories = current_user.categories.order(title: :asc) }
-      end
-    else
-      redirect_to root_path
+    respond_to do |format|
+      format.html { render template: 'users/show' }
+      format.json { @categories = current_user.categories.order(title: :asc) }
     end
   end
 
   def create
     @category = Category.new(category_params)
-    if current_user
-      respond_to do |format|
-        if current_user.categories.find_by(title: @category.title) || @category.title == ""
-          format.json { render nothing: true, status: 422 }
-        else
-          @category.user = current_user
-          if @category.save
-            format.json { render nothing: true, status: 201 }
-          end
+    respond_to do |format|
+      if current_user.categories.find_by(title: @category.title) || @category.title == ""
+        format.json { render nothing: true, status: 422 }
+      else
+        @category.user = current_user
+        if @category.save
+          format.json { render nothing: true, status: 201 }
         end
       end
-    else
-      render nothing: true, status: 401
     end
   end
 
   def update
-    if current_user
-      @category = get_category
-      respond_to do |format|
-        if params[:category][:title] != ""
-          if @category.update_attributes(category_params)
-            format.json { render nothing: true, status: 202 }
-          end
-        else
-          format.json { render json: @category.errors, status: 422 }
+    @category = get_category
+    respond_to do |format|
+      if params[:category][:title] != ""
+        if @category.update_attributes(category_params)
+          format.json { render nothing: true, status: 202 }
         end
+      else
+        format.json { render json: @category.errors, status: 422 }
       end
-    else
-      render nothing: true, status: 401
     end
   end
 
   def destroy
-    if current_user
-      @category = get_category
-      @category.destroy
-      render nothing: true, status: 200
-    else
-      render nothing: true, status: 401
-    end
+    @category = get_category
+    @category.destroy
+    render nothing: true, status: 200
   end
 
   private
